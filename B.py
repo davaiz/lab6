f28ad8004b5e87e866dcb214933f195e339e3014
davaiz <davaizilg@gmail.com>
2015-10-10 02:49:36 +0300

    Update B.py

--- B.py
+++ B.py
@@ -1,20 +1,23 @@
-k=0 
-L=0 
-input=open('input.txt','r') 
-output=open('output.txt','w') 
-n=int(input.readline()) 
-a=input.readline().split() 
-for i in range(n): 
-    kup=int(a[i]) 
-    kup=(kup-5)//5 
-    if kup==0: 
-        k-=1 
-    elif kup<(-k): 
-        k+=kup 
-    else:  
-        L+=k+kup
-        k = 0 
-print(L, file = output) 
+inp = open("input.txt",r) 
+otp = open("output.txt",w) 
+N = int(inp.readline())
+A = inp.readline().split()
+k = 0
+L = 0
+for i in range(N):
+    i = int(A[i])
+    if i == 5:
+        k += 1
+    else:
+        if (i-5)//5 > k:
+            L += (i-5)//5 - k
+            k = 0
+        else:
+            k -= (i-5)//5
+otp.write(str(L))
+inp.close()
+otp.close()
+
 
      
     
