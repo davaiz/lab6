0fe72ff0ee3c28074b8bd39d515f335eb5e71bd3
davaiz <davaizilg@gmail.com>
2015-10-09 17:39:17 +0300

    A

--- A.py
+++ A.py
@@ -4,7 +4,9 @@ N = int(input())
 A = list(map(int, input().split()))
 for i in A: 
     if A.count(i) == 2:
-        pritn(i, file=output)
+        output.write(i)
         break
+input.close()
+output.close()
 
       
