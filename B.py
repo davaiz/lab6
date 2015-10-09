c96c71bb94ceefc2a43a80dd4ba3dce4c6d11f41
davaiz <davaizilg@gmail.com>
2015-10-10 02:13:40 +0300

    Update B.py

--- B.py
+++ B.py
@@ -42,27 +42,23 @@
 | 50 5 5 5 |       |
 +----------+-------+
 '''
-inp = open("input.txt",r) 
-otp = open("output.txt",w) 
-N = int(inp.readline())
-A = inp.readline().split()
-k = 0
-L = 0
-for i in range(N):
-    i = int(A[i])
-    if i == 5:
-        k += 1
-    else:
-        if (i-5)/5 > k:
-            L += (i-5)/5 - k
-            k = 0
-        else:
-            k -= (i-5)/5
-otp.write(str(L))
-inp.close()
-otp.close()
+k=0 
+l=0 
+input=open('input.txt','r') 
+output=open('output.txt','w') 
+n=int(input.readline()) 
+a=input.readline().split() 
+for i in range(n): 
+    kupura=int(a[i]) 
+    kupura=(kupura-5)//5 
+    if kupura==0: 
+        k-=1 
+    elif kupura<(-k): 
+        k+=kupura 
+    else:  
+        l+=k+kupura
+        k=0 
+print(l,file=output) 
 
-
-     
      
     
