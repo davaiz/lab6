796491575741f3d6210d49b18a406e144cafc01c
davaiz <davaizilg@gmail.com>
2015-10-10 03:15:59 +0300

    Update C.py

--- C.py
+++ C.py
@@ -31,14 +31,14 @@ out = open('output.txt','w')
 N = int(input.readline())
 A = input.readline().split()
 for i in range(N):
-    A[i] = int(A[i])
+  A[i] = int(A[i])
 m = N + 1
 for i in range(N):
-    if((A[i]) < 0) and (A[i+1:].count(-A[i]) > 0):
-        q = A.index(-A[i])-i
-        if q < m:
-            m = q
+  if((A[i]) < 0) and (A[i+1:].count(-A[i]) > 0):
+    q = A.index(-A[i])-i
+    if q < m:
+      m = q
 if m == n + 1:
-    print(0, file=out)
+  print(0, file=out)
 else:
-    print(m, file=out)
+  print(m, file=out)
