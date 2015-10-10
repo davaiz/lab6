17862bec1764e12b65eb4d17abd23a20e1968688
davaiz <davaizilg@gmail.com>
2015-10-10 03:09:51 +0300

    Update C.py

--- C.py
+++ C.py
@@ -25,3 +25,20 @@
   | -40 41 -42 -41 42 40 |       |
   +----------------------+-------+
 '''
+q = 0
+inp = open('input.txt','r')
+out = open('output.txt','w')
+N = int(input.readline())
+A = input.readline().split()
+for i in range(N):
+  A[i] = int(A[i])
+m = N + 1
+for i in range(N):
+  if((A[i]) < 0) and (A[i+1:].count(-A[i]) > 0):
+      q = A.index(-A[i])-i
+  if q < m:
+    m = q
+if m == n + 1:
+  print(0, file=out)
+else:
+  print(m, file=out)
