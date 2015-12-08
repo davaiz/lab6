c5e1bc639062d793b83d7022676a1c49aaef0e47
davaiz <davaizilg@gmail.com>
2015-12-08 09:45:45 +0300

    Update E.py

--- E.py
+++ E.py
@@ -38,3 +38,17 @@
 | 3 1 100 |            |
 +---------+------------+
 '''
+input=open('input.txt', 'r')
+output=open('output.txt', 'w')
+data = list(map(int, input.readline().split()))
+N = dt[0]
+K = dt[1]
+rest = [0 for i in range(N)]
+for i in range(K):
+    vvod = list(map(int, input.readline().split()))
+    doljnik = vvod[0]-1
+    creditor = vvod[1]-1
+    summa = vvod[2]
+    rest[doljnik] -= summa
+    rest[creditor] += summa
+print(' '.join(map(str, rest)), file=output)
